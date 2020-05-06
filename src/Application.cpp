b6c3713d40fccbf9ce8c228e67990a9dee8457b3
whateverton <evertonandradevilela@gmail.com>
2020-05-05 21:32:39 -0300

    Writing a Shader in OpenGL.

--- src/Application.cpp
+++ src/Application.cpp
@@ -3,6 +3,55 @@
 
 #include <iostream>
 
+static unsigned int CompilerShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type); // Creates empty shader obj
+    const char* src = source.c_str();
+
+    glShaderSource(id, 1, &src, nullptr); // Specify the source (can replace a code to)
+    glCompileShader(id);    // Compiles the previously stored code, turning into an obj
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failde to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+// Function used to create the shaders
+// Receives the actual source code of the shader as strings
+// It returns an unique ID for the shader
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();   // Just creates the object
+    unsigned int vs = CompilerShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompilerShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    // This is just specifies what must be linked
+    glAttachShader(program, vs);    
+    glAttachShader(program, fs);
+    // Here is where we tie objects together.. in this case, shader objects
+    glLinkProgram(program);
+    // Check if the generated program is valid
+    glValidateProgram(program);
+
+    // An obj that is already bound to a program, can be marked to be deleted
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -26,6 +75,8 @@ int main(void)
         std::cout << "Erro!" << std::endl;
 
     std::cout << glGetString(GL_VERSION) << std::endl;
+    std::cout << glGetString(GL_VENDOR) << std::endl;
+    std::cout << glGetString(GL_RENDERER) << std::endl;
 
     float positions[6] = {
         -0.5f, -0.5f,
@@ -47,7 +98,32 @@ int main(void)
     // usa normal, nesse caso não
     // stride - seria o passo.. ou o quanto preciso andar pra chegar no próximo atributo
     // ponteiro pro atributo, é como se fosse um offset_of pra ficar mais claro
-    glVertexAttribPointer(0, 2, GL_FLOAT, false, sizeof(float) * 2, 0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    // in and out?
+
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -63,6 +139,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader);
 
     glfwTerminate();
     return 0;
